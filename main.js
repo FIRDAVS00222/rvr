92862993833bca6c95ae8f866c45d6c718c8c9e8
FIRDAVS00222 <shoirajamilova5@gmail.com>
2024-09-03 14:03:11 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+let object = {name: '' , id:'' , pasword:''  }
+let i1 = prompt('Ismingizni kiriting.')
+let i2 = prompt('Space idingizni kiriting.')
+let i3 = prompt('Space paswordni kiriting.')
+
+console.log(object);
+
+object.name = {i1}
+object.id = {i2}
+object.pasword = {i3}
